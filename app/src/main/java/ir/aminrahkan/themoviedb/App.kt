1fa0aa4a12a4fea3b5a25c57f0ea7431fc4a6a95
Amin Rahkan <amin.rahkan7@gmail.com>
2022-11-30 13:38:39 +0330

    hilt config added

--- app/src/main/java/ir/aminrahkan/themoviedb/App.kt
+++ app/src/main/java/ir/aminrahkan/themoviedb/App.kt
@@ -1,11 +1,13 @@
 package ir.aminrahkan.themoviedb
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 // Developer : Amin Rahkan - Amin.Rahkan7@gmail.com
 // Date : 11/30/22 - 2022
 // Project name : TheMovieDB
 
 
+@HiltAndroidApp
 class App : Application() {
 }
