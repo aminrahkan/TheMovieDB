f544142a29376cd16d6f9d716621ffda8f67d901
Amin Rahkan <amin.rahkan7@gmail.com>
2022-11-30 13:24:21 +0330

    clean modules added

---
+++ app/src/main/java/ir/aminrahkan/themoviedb/App.kt
@@ -0,0 +1,11 @@
+package ir.aminrahkan.themoviedb
+
+import android.app.Application
+
+// Developer : Amin Rahkan - Amin.Rahkan7@gmail.com
+// Date : 11/30/22 - 2022
+// Project name : TheMovieDB
+
+
+class App : Application() {
+}
