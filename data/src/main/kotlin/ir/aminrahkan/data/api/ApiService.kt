1766203f12a2c0df5fdb0c22d274642f95ab7954
Amin Rahkan <amin.rahkan7@gmail.com>
2022-12-07 22:36:56 +0330

    some edit

--- data/src/main/kotlin/ir/aminrahkan/data/api/ApiService.kt
+++ data/src/main/kotlin/ir/aminrahkan/data/api/ApiService.kt
@@ -1,5 +1,8 @@
 package ir.aminrahkan.data.api
 
+import okhttp3.Response
+import retrofit2.http.GET
+
 
 // Developer : Amin Rahkan - Amin.Rahkan7@gmail.com  
 // Date : 12/1/22 - 2022
@@ -7,4 +10,7 @@ package ir.aminrahkan.data.api
 
 
 interface ApiService {
+
+    @GET("/movie/popular")
+    suspend fun getPopularVideos():Response<>
 }
