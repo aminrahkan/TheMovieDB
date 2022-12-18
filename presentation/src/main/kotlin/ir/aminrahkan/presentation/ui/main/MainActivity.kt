c8c3020b0b975c1416f09451fbe2109269e3fd50
Amin Rahkan <amin.rahkan7@gmail.com>
2022-12-18 03:49:15 +0330

    edit

--- presentation/src/main/kotlin/ir/aminrahkan/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/kotlin/ir/aminrahkan/presentation/ui/main/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.aminrahkan.presentation.R
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
